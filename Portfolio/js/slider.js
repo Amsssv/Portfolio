ceac3cc3c58b00a07034ae4aec4bd940759eb955
Amsssv <Amsssv@yandex.ru>
2022-03-14 16:23:03 +0300

    added prev button

--- Portfolio/js/slider.js
+++ Portfolio/js/slider.js
@@ -1,19 +1,48 @@
 (function () {
-	const Button = document.querySelector('.carousel-control-next');
+	const ButtonNext = document.querySelector('.carousel-control-next');
 	const caruselItem = document.querySelectorAll('.carousel-item');
+	const ButtonPrev = document.querySelector('.carousel-control-prev');
 	let i = 0
-	Button.addEventListener('click', function () {
+	ButtonNext.addEventListener('click', function () {
 		if (caruselItem[i].classList.contains('active')) {
 			caruselItem[i].classList.remove('active');
 		} 
 		caruselItem[i+1].classList.add('active');
 		i++;
-		if (i >= caruselItem.length) {
-			 i = 0;
+		});
+		ButtonPrev.addEventListener('click', function () {
+
+		if (caruselItem[i].classList.contains('active')) {
+			caruselItem[i].classList.remove('active');
+		} 
+		caruselItem[i-1].classList.add('active');
+		i--;
+		if (i > 2) {
+			i = 0;
 		}
 	});
 })()
 
+// if (caruselItem[i].classList.contains('active')) {
+// 	caruselItem[i].classList.remove('active');
+// } 
+// caruselItem[i+1].classList.add('active');
+// i++;
+// });
+// ButtonPrev.addEventListener('click', function () {
+
+// if (caruselItem[i].classList.contains('active')) {
+// 	caruselItem[i].classList.remove('active');
+// } 
+// caruselItem[i-1].classList.add('active');
+// i--;
+// });
 
 
-
+// i++;
+// console.log(i);
+// if (i < caruselItem.length) {
+	
+// } else {
+// 	i = 0;
+// }
