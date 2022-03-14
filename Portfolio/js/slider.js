8d7ee252e579e3594bfb1c03acf3393c20982713
Amsssv <Amsssv@yandex.ru>
2022-03-14 09:44:07 +0300

    added and remove classes

--- Portfolio/js/slider.js
+++ Portfolio/js/slider.js
@@ -1,10 +1,16 @@
 (function () {
 	const Button = document.querySelector('.carousel-control-next');
 	const caruselItem = document.querySelectorAll('.carousel-item');
-	var i = 0
+	let i = 0
 	Button.addEventListener('click', function () {
+		if (caruselItem[i].classList.contains('active')) {
+			caruselItem[i].classList.remove('active');
+		} 
+		caruselItem[i+1].classList.add('active');
 		i++;
-		caruselItem[i].classList.add('active');
+		if (i >= caruselItem.length) {
+			 i = 0;
+		}
 	});
 })()
 
